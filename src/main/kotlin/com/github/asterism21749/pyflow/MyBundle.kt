e3d666c37e24a82e21ada9ee2b182ab55d8f8a8e
GitHub Action <action@github.com>
2024-06-22 14:19:31 +0000

    Template cleanup

--- src/main/kotlin/com/github/asterism21749/pyflow/MyBundle.kt
+++ src/main/kotlin/com/github/asterism21749/pyflow/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.asterism21749.pyflow
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/asterism21749/pyflow/services/MyProjectService.kt
+++ src/main/kotlin/com/github/asterism21749/pyflow/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.asterism21749.pyflow.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.asterism21749.pyflow.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
